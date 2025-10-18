5db93d2d7fb3ac6ece4610190a1d677f52e6de36
Narek Khachatryan <naroarmenia9@gmail.com>
2025-10-18 15:47:48 +0400

    18.10.2025

---
+++ mallocwrapper.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+template <typename T>
+void xmalloc(size_t size, T*& ptr) {
+    ptr = (T*)malloc(size * sizeof(T));
+    if(ptr == NULL) {
+        std::cout << "Memory allocation failed!" << std::endl;
+    }
+}
+
+template <typename T>
+void xfree(T*& ptr) {
+    if(ptr != NULL) {
+        free(ptr);
+    }
+    ptr = nullptr;
+}
+
+int main() {
+    int* ptr = nullptr;
+    xmalloc(10, ptr);
+    std::cout << "Memory allocated!" << std::endl;
+
+    xfree(ptr);
+    std::cout << "Memory freed!" << std::endl;
+}
