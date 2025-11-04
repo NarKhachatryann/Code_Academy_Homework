a1c89834d2aa66e862f45a3754908188b41eca33
NaroArmenia <naroarmenia9@gmail.com>
2025-11-04 23:29:35 +0400

    04.11.2025

--- matrix/main.cpp
+++ matrix/main.cpp
@@ -1,6 +1,8 @@
 #include "matrix.h"
+#include <fstream>
 
 int main() {
+    std::ofstream outfile("matrix_output.txt", std::ios::app);
     int size = 0;
     std::cout << "Enter size of matrix: ";
     std::cin >> size;
@@ -8,9 +10,16 @@ int main() {
     Matrix mat(size);
     mat.initRandom();
     mat.print();
+    if(outfile.is_open()) {
+        outfile << "Initial Matrix: \n" << mat << std::endl;
+    }
 
     mat = mat * 5;
-    std::cout << "Matrix: \n" << mat << std::endl; 
+    std::cout << "Matrix: \n" << mat << std::endl;
+    if(outfile.is_open()) {
+        outfile << "Matrix: \n" << mat << std::endl;
+        outfile.close();
+    }
 
     return 0;
 }
