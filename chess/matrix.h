c4b20c3dfa167ce62adabcd142815322bfc47f73
NaroArmenia <naroarmenia9@gmail.com>
2025-12-03 15:08:03 +0400

    03.12.2025

--- chess/matrix.h
+++ chess/matrix.h
@@ -2,27 +2,42 @@
 #define MATRIX_H
 
 #include <iostream>
+#include <stdexcept>
+#include <algorithm>
 
+template <typename T>
 class Matrix {
 private:
-    char** m_data;
+    T** m_data; 
     int m_size;
 
 public:
-    Matrix(int size);
+    explicit Matrix(int size = 0);
     Matrix(const Matrix& other);
+    Matrix(Matrix&& other) noexcept;
     virtual ~Matrix();
 
     Matrix& operator=(const Matrix& other);
     Matrix& operator=(Matrix&& other) noexcept;
-    Matrix(Matrix&& other) noexcept;
 
-    void pasteValue(char value, int row, int col);
-    char getElement(int row, int col) const;
-    void setElement(int row, int col, char value);
+    void setElement(int row, int col, T value);
+    T getElement(int row, int col) const;
     int getSize() const;
-
-    friend std::ostream& operator<<(std::ostream& os, const Matrix& obj);
+    
+    friend std::ostream& operator<<(std::ostream& os, const Matrix<T>& obj) {
+        for (int i = 0; i < obj.m_size; ++i) {
+            for (int j = 0; j < obj.m_size; ++j) {
+                os << obj.m_data[i][j] << "\t"; 
+            }
+            os << "\n";
+        }
+        return os;
+    }
+    
+private:
+    void allocate_memory(int size);
+    void deallocate_memory();
+    void copy_data(const Matrix& other);
 };
 
 #endif
